89603e9d5ad68229f84e96e8d8bbe5e5d76595eb
vorillaz <theodorevorillas@gmail.com>
2023-11-27 16:56:03 +0200

    fix: extend tsconfig and prepare tsup

---
+++ tsup.config.ts
@@ -0,0 +1,28 @@
+import type { Options } from 'tsup';
+import { defineConfig } from 'tsup';
+
+export function createTsupConfig({
+  entry = ['src/index.ts'],
+  external = [],
+  format = ['esm', 'cjs'],
+  target = 'es2022',
+  clean = true,
+  outDir = 'dist',
+  minify = true,
+  dts = true,
+  sourcemap = true,
+  ...args
+}: Options = {}) {
+  return defineConfig({
+    entry,
+    external,
+    format,
+    target,
+    clean,
+    outDir,
+    minify,
+    dts,
+    sourcemap,
+    ...args,
+  });
+}
